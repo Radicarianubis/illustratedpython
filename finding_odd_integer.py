4509b53408f6dafe0be98a9143ef2bb58d3e41a1
Ron <radicarianubis@gmail.com>
2018-08-28 20:09:32 -0400

    Using two nested loops to find the odd number of an integer in an array

---
+++ finding_odd_integer.py
@@ -0,0 +1,27 @@
+'''
+def find_it(seq):
+    length = len(seq)
+    for i in range(0, length):
+        count = 0
+        for j in range(0, length):
+            if seq[i] == seq[j]:
+                count += 1
+        if (count % 2 != 0):
+            oddint = seq[i]
+            print(f"Found, the number is {oddint}")
+            return oddint
+    return
+'''
+
+import operator
+import functools
+
+def find_it(xs):
+    return reduce(operator.xor, xs)
+
+
+
+
+
+find_it([20,1,-1,2,2,3,3,5,5,1,2,4,20,4,-1,2,2])
+
